01a03ad85cfc92ffacb530f3796e7906d350a951
LaknaSandeepani <92353904+LaknaSandeepani@users.noreply.github.com>
2023-09-13 20:30:38 +0530

    Initial Commit

---
+++ ss/ssApp.swift
@@ -0,0 +1,17 @@
+//
+//  ssApp.swift
+//  ss
+//
+//  Created by Lakna Attigala on 2023-09-13.
+//
+
+import SwiftUI
+
+@main
+struct ssApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
